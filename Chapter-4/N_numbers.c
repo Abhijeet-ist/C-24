678b4bf7082ead5fdf06989a0d2b51ef9cd9b81b
Abhijeet Kumar <kumarabhijeet4132@gmail.com>
2025-01-07 02:25:54 +0530

    Initial Commit

---
+++ Chapter-4/N_numbers.c
@@ -0,0 +1,14 @@
+// Write a program to print first ‘n’ natural number using do-while loop.
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    int i=0;
+    do{
+        printf("The value of I is: %d\n",i);
+        i++;
+    } while(i<=n);
+    return 0;
+}
