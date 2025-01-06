593299541f6ad811db9ab223322e3e7b2a64c50b
Abhijeet Kumar <kumarabhijeet4132@gmail.com>
2025-01-07 03:45:10 +0530

    Initial Commit

--- Chapter-4/Practice/01_Practice.c
+++ Chapter-4/Practice/01_Practice.c
@@ -1,1 +1,14 @@
-heelo
+// Write a program to print multiplication table of a given number n.
+
+#include <stdio.h>
+
+int main(){
+    int n,i;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+     printf("%d X %d = %d\n", n,i,n*i); 
+    }
+    return 0;
+}
