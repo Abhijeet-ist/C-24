a41cf189d44ecc2697dd8eb95951b3fe58c905fd
Abhijeet Kumar <kumarabhijeet4132@gmail.com>
2025-01-21 20:52:14 +0530

    Commit

--- Projects/1_Number_Guessing.c
+++ Projects/1_Number_Guessing.c
@@ -35,41 +35,49 @@ guesses the player used to arrive at the number.*/
 //     return 0;
 // }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
 
-int main()
-{
-    // Initialize random number generator
-    srand(time(0));
+// int main()
+// {
+//     // Initialize random number generator
+//     srand(time(0));
 
-    // Generate random number between 1 and 100
-    int randomNumber = (rand() % 100) + 1;
-    int no_of_guesses = 0;
-    int guessed_number;
+//     // Generate random number between 1 and 100
+//     int randomNumber = (rand() % 100) + 1;
+//     int no_of_guesses = 0;
+//     int guessed_number;
 
-    // Print the random number
-    // printf("Random Number: %d\n", randomNumber);
+//     // Print the random number
+//     // printf("Random Number: %d\n", randomNumber);
 
-    do
-    {
-        printf("Guess the number");
-        scanf("%d", &guessed_number);
-        if(guessed_number>randomNumber){
-            printf("Lower number please!\n");
-        }
-        else if(guessed_number<randomNumber){
-            printf("Higher number please!\n");
-        }
-        else{
-            printf("Congrats!!\n");
-        }
-        no_of_guesses++;
+//     do
+//     {
+//         printf("Guess the number");
+//         scanf("%d", &guessed_number);
+//         if(guessed_number>randomNumber){
+//             printf("Lower number please!\n");
+//         }
+//         else if(guessed_number<randomNumber){
+//             printf("Higher number please!\n");
+//         }
+//         else{
+//             printf("Congrats!!\n");
+//         }
+//         no_of_guesses++;
 
-    } while (guessed_number != randomNumber);
+//     } while (guessed_number != randomNumber);
 
-    printf("You guessed the number in %d guesses", no_of_guesses);
+//     printf("You guessed the number in %d guesses", no_of_guesses);
 
-    return 0;
+//     return 0;
+// }
+
+#include<stdio.h>
+    
+int main() {
+  int a = 248755;
+  printf("%d",((a/10)+(a%10)));
+  return 0;
 }
