d3a80699f04db244f0c2ad1286d2277feff7db83
Abhijeet Kumar <kumarabhijeet4132@gmail.com>
2025-01-21 23:09:37 +0530

    Update 03_Practice.c

--- Chapter-5/Practice/03_Practice.c
+++ Chapter-5/Practice/03_Practice.c
@@ -1,3 +1,21 @@
 // Write a function to calculate force of attraction on a body of mass ‘m’ exerted by
 // earth. Consider g = 9.8m/s2
 // .
+
+#include <stdio.h>
+#define g 9.8
+
+float force(float a){
+    float x;
+    x = a * g;
+    return x;
+
+}
+
+int main(){
+    float a;
+    printf("Enter the mass of the body: ");
+    scanf("%f", &a);
+    printf("The force exerted on the body is: %.2f", force(a));
+    return 0;
+}
