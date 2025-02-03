0796d114a9f591d620ffda2dc6da9d5431e300f9
Abhijeet Kumar <kumarabhijeet4132@gmail.com>
2025-02-03 13:29:22 +0530

    Create hh.c

---
+++ Chapter-6/hh.c
@@ -0,0 +1,45 @@
+// // #include <stdio.h>
+
+// // int main(){
+// //     int x=5;
+// //     if(x<1);//here the statement is ended, therefore the next statement will not be a part of the if statement
+// //     printf("Hello");
+// //     return 0;
+// // }
+
+// // #include <stdio.h>
+
+// // int main(){
+// //     float x=2.3;
+// //     if (x==2.300000){
+// //         printf("Hi");
+
+// //     } else {
+// //         printf("Hello");// because the actual value is 2.30000 so the conditon will got to else statemnet not in if statememnt as for if the value should be 2.3 
+// //     }
+// //     return 0;
+// // }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     int x;
+//     scanf("%d", &x);
+//     if (x%5==0){}
+//     return 0;
+// }
+
+
+
+
+#include <stdio.h>
+
+int main(){
+    for (int x=1; x<=2 ; x++){
+        for (int y=2; y>=1; y--){
+            printf("%d", x*y);
+        }
+    }
+    return 0;
+}
