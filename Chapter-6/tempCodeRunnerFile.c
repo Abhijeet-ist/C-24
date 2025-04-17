9f69d31566e5d1a77edee102331fcb33e03d6196
Abhijeet-Ist <kumarabhijeet4132@gmail.com>
2025-04-17 23:50:31 +0530

    installations

---
+++ Chapter-6/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// int main(){
+//     float x=2.3;
+//     if (x==2.300000){
+//         printf("Hi");
+
+//     } else {
+//         printf("Hello");// because the actual value is 2.30000 so the conditon will got to else statemnet not in if statememnt as for if the value should be 2.3 
+//     }
+//     return 0;
+// }
